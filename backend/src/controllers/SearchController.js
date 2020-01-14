5bb55986707bb872ce658d3a24cb3decd0f51d22
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-14 16:22:30 -0300

    Implementada as consultas com filtro

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,32 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        console.log(request.query);
+        // Buscar todos devs num raio de  10km
+        // Filtrar por tecnologias
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        // Teste: console.log(techsArray);
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
+        });
+
+        return response.json({ devs });
+
+    }
+}

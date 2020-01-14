369bb33f8349e9f250034ca8eabea46e9efd29bf
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-14 11:21:49 -0300

    Aplicado os RESTful

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello Omnistack!' });
+app.use(express.json());
+
+// Métodos HTTP: get, post, put, delete
+
+// Tipos de parâmetros: 
+// Query Params: req.query (Filtros, ordenação, paginação...)
+// Routs Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    return response.json({ message: 'Hello Dev!' });
 });
 
 app.listen(3333);

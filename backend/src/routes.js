7a2e3a431a7109be9588bb66eef3bdd30fdd61e5
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-14 15:09:48 -0300

    Reorganizada as rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,37 +1,8 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-    // console.log(request.body);
-    const { github_username, techs, latitude, longitude } = request.body;
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-
-    // Exemplo 1:  console.log(apiResponse.data);
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-    // Exemplo 2: console.log(name, avatar_url, bio, github_username);
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-    };
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location,
-    });
-
-    return response.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;

ab3c47a2c6b311cdf33183846f04391c87aa583d
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-14 11:47:21 -0300

    Conectado ao banco de dados MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:nirvanA10@cluster0-uuaaw.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
@@ -10,6 +16,8 @@ app.use(express.json());
 // Query Params: req.query (Filtros, ordenação, paginação...)
 // Routs Params: request.params (Identificar um recurso na alteração ou remoção)
 // Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Banco de dados Não-relacional)
 
 app.post('/users', (request, response) => {
     console.log(request.body);

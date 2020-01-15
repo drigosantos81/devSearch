e5010a2c3a207ab6351b27ef184642b43be674ff
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-15 13:31:54 -0300

    Implementado o Sidebar HTML-CSS

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
 
-// Componente: Bloco isolado de HTML, CSS, JS que não interfere no restante da aplicação.
-// Propriedade: Informações que um componente PAI passa para o componente FILHO.
-// Estado: Informação mantidas pelo componente (Lembrar: Imutabilidade).
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="App">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label> 
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label> 
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label> 
+              <input name="longitude" id="longitude" required />
+            </div>          
+          </div>
+          
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
   );
 }
 

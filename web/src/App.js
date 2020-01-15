5e1f9320fce97916875dcdce16ff3ab594660c6e
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-15 11:18:35 -0300

    Exemplo do conceito de Estado

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Componente: Bloco isolado de HTML, CSS, JS que não interfere no restante da aplicação.
 // Propriedade: Informações que um componente PAI passa para o componente FILHO.
-// Estado: 
-
-import Header from './Header';
+// Estado: Informação mantidas pelo componente (Lembrar: Imutabilidade).
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter + 1);
+  }
   return (
     <>
-      <Header title="Título 1" />
-      <Header title="Título 2" />
-      <Header title="Título 4" />
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
     </>
   );
 }

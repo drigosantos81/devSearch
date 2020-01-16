036ee2771a6cf2d10f9f75b9f292edb2a3a4e74f
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-16 16:02:43 -0300

    input de busca e ícone localização implementados

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -34,7 +35,8 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
             <Marker coordinate={{ latitude: -12.9609619, longitude: -38.4953068}}>
                 <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/52610125?s=460&v=4' }} />
 
@@ -49,6 +51,20 @@ function Main({ navigation }) {
                 </Callout>
             </Marker>
         </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}    
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color={"#FFF"} />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -81,6 +97,42 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
     },
 })
 
